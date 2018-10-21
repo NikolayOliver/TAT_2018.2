f53827daf06ee13107e759a488bb4442f4cf0dac
Nikolay Korolev <nikolayoliver@gmail.com>
2018-10-21 12:11:24 +0300

    HomeTask 2 - translate symbol

---
+++ HomeTask2/HomeTask2/CheckingAndTranslateSymbols.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask2
+{
+    class CheckingAndTranslateSymbols
+    {
+        // this function check each input symbol
+        // return true, if all symbol is letter
+        // else return false
+        public bool CheckingSymbols(string[] args)
+        {
+            char[] argToChar;
+            foreach(string arg in args)
+            {
+                argToChar = arg.ToCharArray();
+                foreach(char symbol in argToChar)
+                {
+                    if(char.IsLetter(symbol) == false)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        // function translate russian letters in english
+        // and back
+        //public void Translate(string arg, Dictionary<char, string> translationDictionary, StringBuilder stringTranslate)
+        //{
+        //    for (int i = 0; i < arg.Length; i++)
+        //    {
+        //        foreach (KeyValuePair<char, string> dictionary in translationDictionary)
+        //        {
+        //            if (dictionary.Key == arg.ElementAt(i))
+        //            {
+        //                stringTranslate.Append(dictionary.Value);
+        //            }
+        //        }
+        //    }
+            
+        //}
+        public string Translate(char symbol, Dictionary<char, string> translationDictionary)
+        {
+            foreach (KeyValuePair<char, string> dictionary in translationDictionary)
+            {
+                if (dictionary.Key == symbol)
+                {
+                    return dictionary.Value;
+                }
+                
+            }
+            return "$$";
+        }
+    }
+
+ 
+}
---
+++ HomeTask2/HomeTask2/TranslationLetter.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask2
+{
+    class TranslationLetter
+    {
+        static void Main(string[] args)
+        {
+            char[] symbols;
+            Dictionary<char, string> translationDictionary = new Dictionary<char, string>()
+            {
+                // Russian -> English
+               {'А',"A"}, {'Б',"B"}, {'В',"V"},
+               {'Г',"G"}, {'Д',"D"}, {'Е',"E"},
+               {'Ё',"YO"}, {'Ж',"ZH"}, {'З',"Z"},
+               {'И',"I"}, {'Й',"Y"}, {'К',"K"},
+               {'Л',"L"}, {'М',"M"}, {'Н',"N"},
+               {'О',"O"}, {'П',"P"}, {'Р',"R"},
+               {'С',"S"}, {'Т',"T"}, {'У',"U"},
+               {'Ф',"F"}, {'Х',"KH"}, {'Ц',"TS"},
+               {'Ч',"CH"}, {'Ш',"SH"}, {'Щ',"SCH"},
+               {'Ъ',"#"}, {'Ы',"Y"}, {'Ь',"#"},
+               {'Э',"E"}, {'Ю',"YU"}, {'Я',"YA"},
+                // English -> Russian
+               {'A',"А"}, {'B',"Б"}, {'C',"Ц"},
+               {'D',"Д"}, {'E',"Е"}, {'F',"Ф"},
+               {'G',"ДЖ"}, {'H',"Х"}, {'I',"АЙ"},
+               {'J',"Ж"}, {'K',"К"}, {'L',"Л"},
+               {'M',"М"}, {'N',"Н"}, {'O',"О"},
+               {'P',"П"}, {'Q',"КЬЮ"}, {'R',"Р"},
+               {'S',"С"}, {'T',"Т"}, {'U',"Ю"},
+               {'V',"В"}, {'W',"ВИ"}, {'X',"ИСК"},
+               {'Y',"И"}, {'Z',"З"}
+            };
+            CheckingAndTranslateSymbols checkAndTranslate = new CheckingAndTranslateSymbols();
+            Console.WriteLine("Input string consisting of letters");
+            string newArg;
+            while(true)
+            {
+                if(args.Length == 0)
+                {
+                    Console.WriteLine("You must to input string");
+                }
+                if(args.Length > 1)
+                {
+                    Console.WriteLine("Input only letter! \nTry again");
+                }
+                if(args.Length == 1)
+                {
+                    if(checkAndTranslate.CheckingSymbols(args) == true)
+                    break;
+                }
+                newArg = Console.ReadLine();
+                args = newArg.Split(' ');
+            }
+            
+            StringBuilder translateString =  new StringBuilder(); 
+            string str;
+
+            foreach (string arg in args)
+            {
+                symbols = arg.ToCharArray();
+                for (int i = 0; i < symbols.Length; i++)
+                {
+                    translateString.Append(checkAndTranslate.Translate(symbols[i], translationDictionary));
+                }
+            }
+            str = translateString.ToString();
+            Console.WriteLine(str);
+
+            //string str = "sjdfs";
+            //str = str.ToUpper();
+            //str = str.Replace('D', 'д');
+            //char ch = 'd';
+            //if(Char.IsLetter('Б')==true)
+            //{
+            //    Console.WriteLine("yes");
+            //}
+           
+            //Console.WriteLine(str.ElementAt(2));
+            Console.ReadKey();
+        }
+
+    }
+}
