221bec2539067e669f24ac22d2ca2da42ad3b460
Nikolay Korolev <nikolayoliver@gmail.com>
2018-11-04 17:28:20 +0300

    Cars Patern command

---
+++ Task_dev_5/Task_dev_5/CountAllHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_dev_5
+{
+    // Calculate and write count of all cars
+    // field - countAll - count of all cars
+    class CountAllHandler
+    {
+        int countAll = 0;
+        // Calculate number of all cars
+        public CountAllHandler(List<string> optionsCars)
+        {
+            // 2 - first appearance of the number of cars, 4 - total command
+            for(int i = 2; i < optionsCars.Count; i+=4)
+            {
+                countAll += int.Parse(optionsCars[i]); 
+            }
+        }
+        public void CountAll()
+        {
+            Console.WriteLine(countAll);
+        }
+    }
+    // 
+    class CountAllCommand:ICommand
+    {
+        CountAllHandler countAll;
+        public CountAllCommand(CountAllHandler cntAll)
+        {
+            countAll = cntAll;
+        }
+        public void Execute()
+        {
+            countAll.CountAll();
+        }
+    }
+}
---
+++ Task_dev_5/Task_dev_5/EntryPoint.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_dev_5
+{
+    class EntryPoint
+    {
+        // this enum for (switch case)
+        enum command
+        {
+            countTypes = 1,
+            countAll,
+            averagePrice,
+            averagePriceType,
+            exit
+        }
+        // Entry Point
+        static int Main(string[] args)
+        {
+            try
+            {   // file path
+                string path = @"c:\Users\User\Source\Repos\TAT_2018.2\Task_dev_5\Task_dev_5\List of Cars.txt";
+                var carsAdder = new CarsAdder();
+                string brandPrice;
+                List<string> carOptions = carsAdder.CarsAdd(path);
+                CountTypesHandler countTypes = new CountTypesHandler(carOptions);
+                CountAllHandler countAll = new CountAllHandler(carOptions);
+                AveragePriceHandler averagePrice = new AveragePriceHandler(carOptions);
+                Invoker invoker = new Invoker();
+                int chooser;
+                while (true) 
+                {
+                    chooser = int.Parse(Console.ReadLine());
+                    // checking to exit
+                    if(chooser == 5)
+                    {
+                        break;
+                    }
+                    switch ((command)chooser)
+                    {
+                        case command.countTypes:
+                            invoker.SetCommand(new Command(new CountTypesCommand(countTypes)));
+                            invoker.DoCommandFunction();
+                            break;
+                        case command.countAll:
+                            invoker.SetCommand(new Command(new CountAllCommand(countAll)));
+                            invoker.DoCommandFunction();
+                            break;
+                        case command.averagePrice:
+                            invoker.SetCommand(new Command(new AveragePriceCommand(averagePrice)));
+                            invoker.DoCommandFunction();
+                            break;
+                        case command.averagePriceType:
+                            brandPrice = Console.ReadLine();
+                            // create here, because need input which cars brand want to see client
+                            AveragePriceTypeHandler averagePriceType = new AveragePriceTypeHandler(carOptions, brandPrice);
+                            invoker.SetCommand(new Command(new AverageTypePriceCommand(averagePriceType)));
+                            break;
+                        default:
+                            throw new Exception("No such option");
+                    }
+                }
+                return 0;
+                
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return -1;
+            }
+    
+        }
+    }
+}
